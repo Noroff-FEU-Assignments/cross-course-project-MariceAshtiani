7a2b28432ec5fe1431bfe4bb97e76bb356441f1b
MariceAshtiani <marice95@hotmail.no>
2022-08-28 13:34:50 +0200

    fetched api for catergory

--- js/men.js
+++ js/men.js
@@ -4,34 +4,34 @@ const proxy = "https://noroffcors.herokuapp.com/";
 
 const corsFix = proxy + url;
 
-const resultsContainer = document.querySelector(".maleproduct-container");
+const productContainer = document.querySelector(".maleproduct-container");
 
 async function getMensProducts() {
 
-        const response = await fetch(corsFix);
+        try {
+                const response = await fetch(corsFix);
+                const results = await response.json();
 
-        const results = await response.json();
+                productContainer.innerHTML = "";
 
-        resultsContainer.innerHTML = "";
+                for(let i = 0; i < results.length; i++) {
+                        console.log(results[i]);
 
-        for(let i = 0; i < results.length; i++) {
-            console.log(results[i]);
-
-            resultsContainer.innerHTML += `<div class="maleproduct"> 
-                                            <a class="product-specific" href="stormcoat.html?id=${results[i].id}">
-                                            <h2>${results[i].name}</h2>
-                                            <img src="${results[i].images[0].src}" alt="${results[i].name}" class="productmen"></img>
-                                            <p>${results[i].price_html}</p>
-                                            </a>
-                                            <button id="item-${results[i].id}" class="atc" data-product="${results[i].id}">Add</button>
-                                            </div>
-                                            `
+                        productContainer.innerHTML += `<div class="maleproduct"> 
+                                                        <a class="product-specific" href="stormcoat.html?id=${results[i].id}">
+                                                        <h2>${results[i].name}</h2>
+                                                        <img src="${results[i].images[0].src}" alt="${results[i].name}" class="productmen"></img>
+                                                        <p>${results[i].price_html}</p>
+                                                        </a>
+                                                        <button id="item-${results[i].id}" class="atc" data-product="${results[i].id}">Add</button>
+                                                        </div>
+                                                        `
+                }
         }
-
+        catch(error) {
+                console.log(eror);
+                productContainer.innerHTML = message("error", error);
+        }
 }
 
 getMensProducts();
-
-
-
-
